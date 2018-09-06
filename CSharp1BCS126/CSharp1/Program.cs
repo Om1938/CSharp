24330dbc07f479518871bdc26596a7c651a98374
Om Prakash <omkletu@gmail.com>
2018-09-06 15:02:20 +0530

    intial

---
+++ CSharp1BCS126/CSharp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSharp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n,key;
+            Console.WriteLine("Enter Array Size");
+            n = Convert.ToInt32(Console.ReadLine());
+            array arr = new array(n);
+            arr.setelements();
+            
+            Console.WriteLine("Max Element : " +arr.getMax());
+
+            Console.Write("Enter the search element : ");
+            key = Convert.ToInt32(Console.ReadLine());
+            arr.searchKey(key);
+            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CSharp1BCS126/CSharp1/array.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSharp1
+{
+    class array
+    {
+        int[] arr;
+        int maxELement;
+        int size;
+        public array()
+        {
+            this.size = 10;
+            arr = new int[10];
+        }
+        public array(int size) 
+        {
+            this.size = size;
+            arr = new int[size];
+        }
+        public void setelements()
+        {
+            Console.WriteLine("Enter " + size + " Elements");
+            for (int i = 0; i < size; i++) 
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+                if (maxELement <= arr[i])
+                {
+                    maxELement = arr[i];
+                }
+            }
+        }
+        public int getMax() 
+        {
+            return maxELement;
+        }
+        public void searchKey(int key)
+        {
+            int count = 0;
+            bool flag = false;
+            int[] foundIndex = new int[size];
+
+            for (int i = 0; i < size; i++)
+            {
+                if (arr[i] == key)
+                {
+                    foundIndex[count++] = i;
+                    flag = true;
+                }
+            }
+
+            if (flag == false)
+            {
+                Console.WriteLine("Not found");
+            }
+            else
+            {
+                Console.WriteLine(key + " found " + count +" times.");
+                Console.Write("Location/s : ");
+
+                for (int i = 0; i < count; i++)
+                {
+                    Console.Write(foundIndex[i]+" ");
+                }
+            }
+        }
+
+        
+    }
+}
